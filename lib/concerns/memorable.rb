3671228d93c18d66e6cd64afd883916f0c5829bf
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2017-10-17 19:04:12 +0000

    automatically backed up by learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -3,15 +3,15 @@ module Memorable
     def reset_all
       self.all.clear
     end
- 
+
     def count
       self.all.count
     end
   end
- 
+
   module InstanceMethods
     def initialize
-      self.class.all << self 
+      self.class.all << self
     end
   end
 end
